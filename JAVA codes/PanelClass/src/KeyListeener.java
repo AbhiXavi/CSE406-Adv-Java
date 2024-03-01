414f8acc939e5cb75a9092332afaf3710c70a729
Abhi's Git <akapudasi@gmail.com>
2024-03-01 11:44:09 +0530

    new

--- JAVA codes/PanelClass/src/KeyListeener.java
+++ JAVA codes/PanelClass/src/KeyListeener.java
@@ -1,3 +1,21 @@
-public class KeyListeener {
+import javax.swing.*;
+import java.awt.*;
 
+class MyFrame3 extends JFrame {
+    Container c;
+    MyFrame3() {
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        c = getContentPane();
+        c.setLayout(null);
+
+        setVisible(true);
+    }
+}
+public class KeyListeener {
+    public static void main(String[] args) {
+
+    }
 }
